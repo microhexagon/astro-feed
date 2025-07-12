84e56d2455cb1b9a6e10d9d3c5735d0d9c92ae5b
aima kareem <amikareem921@gmail.com>
2025-07-12 23:14:13 +0500

    feat: add footer component with links and styling

--- astrofeed-next/app/components/Footer.tsx
+++ astrofeed-next/app/components/Footer.tsx
@@ -1,5 +1,26 @@
-// export default function Footer() {
-//     return (
-//             <div>Explore More Section</div>
-//     )
-// }
+'use client';
+import { useState } from "react";
+import { FiTwitter } from "react-icons/fi";
+import { FaInstagram } from "react-icons/fa6";
+import { TiSocialFacebookCircular } from "react-icons/ti";
+export default function Footer() {
+    
+    return (
+            <footer>
+                <div className="flex justify-center gap-22 m-3 text-gray-500 font-medium ">
+                    <a href="">About</a>
+                    <a href="">Contact</a>
+                    <a href="">Privacy Policy</a>
+                    <a href="">Term of services</a>
+                </div>
+                <div className="flex justify-center gap-4 text-gray-500 m-3 font-medium ">
+                    <a href=""><FiTwitter className="" /></a>
+                    <a href=""><FaInstagram /></a>
+                    <a href=""><TiSocialFacebookCircular /></a>
+                </div>
+                <div className="flex justify-center text-gray-500 mt-3 mb-3">
+                    @2024 AstroFeed. All rights reserved.
+                </div>
+            </footer>
+    )
+}
--- astrofeed-next/app/page.tsx
+++ astrofeed-next/app/page.tsx
@@ -2,7 +2,7 @@ import Navbar from './components/Navbar';
 // import HeroSection from './components/HeroSection';
 // import Featured from'./components/Featured';
 // import ExploreMore from'./components/ExploreMore';
-// import Footer from './components/Footer';
+import Footer from './components/Footer';
 
 
 export default function Home(){
@@ -11,8 +11,8 @@ export default function Home(){
         <Navbar/>
         {/* <HeroSection/>
         <Featured/>
-        <ExploreMore/>
-        <Footer /> */}
+        <ExploreMore/> */}
+        <Footer />
         </>
     
     )
