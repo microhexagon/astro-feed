4d63abe12b6c9c158c2b3edfc69d8df47a55ad9c
umekalsoom <umekalsoomrizvia@gmail.com>
2025-07-19 21:34:59 +0500

    Interface for organization

--- astrofeed-next/app/Upcoming Lauches/launches.tsx
+++ astrofeed-next/app/Upcoming Lauches/launches.tsx
@@ -4,10 +4,16 @@ import { useState, useEffect } from 'react';
 import Image from 'next/image';
 import { format, isToday, isTomorrow, parseISO } from 'date-fns';
 
-// Interface for launch status information
+// Interface for launch  information
 interface LaunchStatus {
     id: number;
     name: string;
     abbrev: string;
     description: string;
+  }
+  // Interface for organization
+  interface LaunchServiceProvider {
+    id: number;
+    name: string;
+    type: string;
   }
