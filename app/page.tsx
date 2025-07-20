7095c0cbecbf6a407b00fef9556cc1a5b666a265
ZahoorOnly <i.am.zahoor.a@outlook.com>
2025-07-20 18:50:04 +0500

    fix: ui improved

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,13 @@
-import HeroSection from '../components/landing page/HeroSection';
-import Featured from '../components/landing page/Featured'
-import ExploreMore from'../components/landing page/ExploreMore';
+import HeroSection from "../components/HeroSection";
+import Featured from "../components/Featured";
+import ExploreMore from "../components/ExploreMore";
 
-
-export default function Home(){
-    return (
-        <>
-        
-        <div className='pt-10 pr-40 pb-10 pl-40 bg-gray-800 text-white '>
-            <HeroSection/>
-            <Featured/>
-            <ExploreMore/>
-        </div>
-        </>
-    )
+export default function Home() {
+  return (
+    <main>
+      <HeroSection />
+      <Featured />
+      <ExploreMore />
+    </main>
+  );
 }
