72b5b33f3d809d90a457182fe4720742c05b8ac7
ZahoorOnly <i.am.zahoor.a@outlook.com>
2025-07-20 18:29:47 +0500

    fix: removed navbar import from page route

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,16 @@
-import Navbar from './components/Navbar';
-import HeroSection from './landing page/HeroSection';
-import Featured from './landing page/Featured'
-import ExploreMore from'./landing page/ExploreMore';
-import Footer from './components/Footer';
+import HeroSection from '../components/landing page/HeroSection';
+import Featured from '../components/landing page/Featured'
+import ExploreMore from'../components/landing page/ExploreMore';
 
 
 export default function Home(){
     return (
         <>
-        <Navbar links={["APOD","News","Launches","Contact","About"]} />
+        
         <div className='pt-10 pr-40 pb-10 pl-40 bg-gray-800 text-white '>
             <HeroSection/>
             <Featured/>
             <ExploreMore/>
-            <Footer/>
         </div>
         </>
     )
