41501b527f5ea84008dfdcdbd4ff1b19c6796dd9
umekalsoom <umekalsoomrizvia@gmail.com>
2025-07-24 20:13:42 +0500

    install giest to fix footer clr

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,14 @@
+// app/layout.tsx
+
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { GeistSans } from 'geist/font/sans';
+import { GeistMono } from 'geist/font/mono';
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const geistSans = GeistSans;
+const geistMono = GeistMono;
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,13 +21,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gray-800`}
-      >
-        <Navbar links={["APOD", "News", "Launches", "Contact", "About"]} />
-        {children}
-        <Footer />
+    <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body>
+       
+        <div className="bg-[#1A202C] text-[#a0aec0] min-h-screen">
+          <Navbar links={["Home", "News", "Launches", "Contact", "About"]} />
+          {children}
+          <Footer />
+        </div>
       </body>
     </html>
   );
