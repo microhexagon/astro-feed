3c38c4cdaefbbc05656c8691955d0e8377b6afb7
aima kareem <amikareem921@gmail.com>
2025-09-13 19:14:18 +0500

    Add side margins in page layout

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { Herosection } from "@/components/HeroSection";
 
 export default function Home() {
   return (
-    <main className="text-white">
+    <main className="text-white bg-gray-900 px-4 sm:px-8 md:px-16 lg:px-24">
       <Herosection />
       <Featured />
       <ExploreMore />
